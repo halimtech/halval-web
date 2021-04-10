51be3aaa9c83d6baca372ebbc3228c0158980bff
Abdelhalim <haleem_ah1998@yahoo.com>
2021-04-10 13:37:37 +0200

    adding ssr and tiding shit up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,85 +1,20 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { Provider, createClient, fetchExchange, dedupExchange } from 'urql'
-import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache"
 import theme from '../theme'
-import { LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation } from "../generated/graphql"
 
-function betterUpdate<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, data => fn(result, data as any) as any)
-}
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: 'include',
-  },
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-      Mutation: {
-        logout: (_result, args, cache, info) => {
-          betterUpdate<LogoutMutation, MeQuery>(
-            cache,
-            { query: MeDocument },
-            _result,
-            () => ({ me: null })
-          )
-        },
-
-        login: (_result, args, cache, info) => {
-          betterUpdate<LoginMutation, MeQuery>(cache,
-            { query: MeDocument },
-            _result,
-            (result, query) => {
-              if (result.Login.errors) {
-                return query
-              } else {
-                return {
-                  me: result.Login.user
-                }
-              }
-            }
-          )
-
-        },
-        Register: (_result, args, cache, info) => {
-          betterUpdate<RegisterMutation, MeQuery>(cache,
-            { query: MeDocument },
-            _result,
-            (result, query) => {
-              if (result.register.errors) {
-                return query
-              } else {
-                return {
-                  me: result.register.user
-                }
-              }
-            }
-          )
-
-        }
-      }
-    }
-  }), fetchExchange]
-})
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+
   )
 }
 
---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,60 @@
+import { dedupExchange, fetchExchange } from "urql"
+import { LogoutMutation, MeQuery, MeDocument, LoginMutation, RegisterMutation } from "../generated/graphql"
+import { betterUpdate } from "./betterUpdate"
+import { cacheExchange } from "@urql/exchange-graphcache"
+
+
+export const createUrqlClient = (ssrExchange: any) => ({
+    url: "http://localhost:4000/graphql",
+    fetchOptions: {
+        credentials: 'include' as const,
+    },
+    exchanges: [dedupExchange, cacheExchange({
+        updates: {
+            Mutation: {
+                logout: (_result, args, cache, info) => {
+                    betterUpdate<LogoutMutation, MeQuery>(
+                        cache,
+                        { query: MeDocument },
+                        _result,
+                        () => ({ me: null })
+                    )
+                },
+
+                login: (_result, args, cache, info) => {
+                    betterUpdate<LoginMutation, MeQuery>(cache,
+                        { query: MeDocument },
+                        _result,
+                        (result, query) => {
+                            if (result.Login.errors) {
+                                return query
+                            } else {
+                                return {
+                                    me: result.Login.user
+                                }
+                            }
+                        }
+                    )
+
+                },
+                Register: (_result, args, cache, info) => {
+                    betterUpdate<RegisterMutation, MeQuery>(cache,
+                        { query: MeDocument },
+                        _result,
+                        (result, query) => {
+                            if (result.register.errors) {
+                                return query
+                            } else {
+                                return {
+                                    me: result.register.user
+                                }
+                            }
+                        }
+                    )
+
+                }
+            }
+        }
+    }), ssrExchange, fetchExchange]
+})
+
